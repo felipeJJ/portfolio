03677a0dae64a1c35de1a3f029065c9095bdb082
felipeJJ <jorgefelipe11@hotmail.com>
2024-07-13 02:22:07 -0300

    fix: removing tailwind classes

--- src/app/components /MainContent.tsx
+++ src/app/components /MainContent.tsx
@@ -2,8 +2,8 @@ import NavBar from './navBar/NavBar'
 
 export default function MainContent() {
     return (
-        <section className="ml-8 w-[80%] overflow-y-scroll rounded-2xl bg-gradient-to-tr from-neutral-800 to-neutral-700">
-            <div className="flex justify-between">
+        <section className="ml-8 w-[80%] overflow-y-hidden rounded-2xl bg-gradient-to-tr from-neutral-800 to-neutral-700 pl-8">
+            <div>
                 <NavBar />
             </div>
         </section>
