9607e6b7a5bed1785a23263f4071aaa85dd4103c
felipeJJ <jorgefelipe11@hotmail.com>
2024-07-14 19:23:35 -0300

    fix: changing tailwind css class 'overflow'

--- src/app/components /MainContent.tsx
+++ src/app/components /MainContent.tsx
@@ -2,7 +2,7 @@ import NavBar from './navBar/NavBar'
 
 export default function MainContent() {
     return (
-        <section className="ml-8 w-[80%] overflow-y-hidden rounded-2xl bg-gradient-to-tr from-neutral-800 to-neutral-700 pl-8">
+        <section className="ml-8 w-[80%] overflow-scroll rounded-2xl bg-gradient-to-tr from-neutral-800 to-neutral-700 pl-10">
             <div>
                 <NavBar />
             </div>
