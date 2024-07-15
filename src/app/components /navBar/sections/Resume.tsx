fe547518b5e1dd925cca10d0eb6cc32b9fb33546
felipeJJ <jorgefelipe11@hotmail.com>
2024-07-15 13:21:21 -0300

    feat: resume section done

--- src/app/components /navBar/sections/Resume.tsx
+++ src/app/components /navBar/sections/Resume.tsx
@@ -1,3 +1,248 @@
+import { IoBookOutline, IoConstructOutline } from 'react-icons/io5'
+
 export default function Resume() {
-    return <div></div>
+    return (
+        <>
+            <div className="mb-10">
+                <div className="mb-2 flex items-center gap-5">
+                    <IoConstructOutline size={22} />
+                    <h3>Experiência</h3>
+                </div>
+                <div className="flex justify-start">
+                    <ul className="timeline timeline-snap-icon timeline-compact timeline-vertical">
+                        <li>
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5 text-amber-300"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end mb-5">
+                                <div className="mb-2 text-lg font-black text-white">
+                                    Técnico de computador - R2 Computadores
+                                </div>
+                                <time className="font-mono italic">
+                                    2019 - 2020
+                                </time>
+                                <p className="font-light text-gray-400">
+                                    Montagem e manutenção de computadores.
+                                </p>
+                            </div>
+                            <hr />
+                        </li>
+                        <li>
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5 text-amber-300"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end mb-5">
+                                <div className="timeline-end mb-5">
+                                    <div className="mb-2 text-lg font-black text-white">
+                                        Técnico de computador - R2 Computadores
+                                    </div>
+                                    <time className="font-mono italic">
+                                        2019 - 2020
+                                    </time>
+                                    <p className="font-light text-gray-400">
+                                        Montagem e manutenção de computadores.
+                                    </p>
+                                </div>
+                            </div>
+                            <hr />
+                        </li>
+                        <li>
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5 text-amber-300"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end mb-5">
+                                <div className="mb-2 text-lg font-black text-white">
+                                    Desenvolvedor web FullSteck - NPC Tech
+                                </div>
+                                <time className="font-mono italic">
+                                    jan/2024 - atuaumente{' '}
+                                </time>
+                                <p className="font-light text-gray-400">
+                                    No frontend Desenvolvo interfaces
+                                    responsivas e compatíveis com Next.js,
+                                    Tailwind CSS, TypeScript. Realizo testes,
+                                    consumo APIs e uso Git para controle de
+                                    versão. No backend Crio lógica de servidor
+                                    APIs REST com Node.js e TypeScript, gerencio
+                                    PostgreSQL e implemento segurança e testes
+                                    com Jest.
+                                </p>
+                            </div>
+                            <hr />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="mb-10">
+                <div className="mb-2 flex items-center gap-5">
+                    <IoBookOutline size={20} />
+                    <h3>Formação</h3>
+                </div>
+                <div className="flex justify-start">
+                    <ul className="timeline timeline-snap-icon timeline-compact timeline-vertical">
+                        <li>
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5 text-amber-300"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end mb-5">
+                                <div className="mb-2 text-lg font-black text-white">
+                                    Escola SENAI de Informática
+                                </div>
+                                <time className="font-mono italic">
+                                    2011 - 2012
+                                </time>
+                                <p className="font-light text-gray-400">
+                                    Manutenção de computadores desktop,
+                                    manutenção manutenção de notebooks, sistemas
+                                    operacionais, manutenção em redes de
+                                    computadores.
+                                </p>
+                            </div>
+                            <hr />
+                        </li>
+                        <li>
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5 text-amber-300"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end mb-5">
+                                <div className="mb-2 text-lg font-black text-white">
+                                    Multivix - Bacharelado em Engenharia da
+                                    Computação
+                                </div>
+                                <time className="font-mono italic">
+                                    2017 - 2024
+                                </time>
+                                <p className="font-light text-gray-400">
+                                    Desenvolvimento de software, arquitetura de
+                                    computadores, algoritmos, estruturas de
+                                    dados,entre outros.
+                                </p>
+                            </div>
+                            <hr />
+                        </li>
+                        <li>
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5 text-amber-300"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end mb-5">
+                                <div className="mb-2 text-lg font-black text-white">
+                                    DIO - Impulso Javascript Evolution
+                                </div>
+                                <time className="font-mono italic">2022</time>
+                                <p className="font-light text-gray-400">
+                                    Bootcamp realziado na plataforma de ensino
+                                    DIO, focada em Desenvolvimento Web
+                                    FullSteck.
+                                </p>
+                            </div>
+                            <hr />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div className="mb-2 flex items-center gap-5">
+                    <IoConstructOutline size={22} />
+                    <h3>Competências</h3>
+                </div>
+                <div className="flex justify-start">
+                    <ul className="timeline timeline-snap-icon timeline-compact timeline-vertical">
+                        <li>
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5 text-amber-300"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end mb-5">
+                                <div className="mb-2 text-lg font-black text-white">
+                                    Inglês - Avançado
+                                </div>
+                            </div>
+                            <hr />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
