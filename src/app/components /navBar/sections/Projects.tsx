a013a800a419c9c6f0899d8017c1553d58691630
felipeJJ <jorgefelipe11@hotmail.com>
2024-07-20 18:52:47 -0300

    feat: adding two projects

--- src/app/components /navBar/sections/Projects.tsx
+++ src/app/components /navBar/sections/Projects.tsx
@@ -1,3 +1,96 @@
+import Image from 'next/image'
+import {
+    BiLogoTypescript,
+    BiLogoTailwindCss,
+    BiLogoNodejs,
+} from 'react-icons/bi'
+import { RiNextjsLine, RiGithubFill } from 'react-icons/ri'
+import { SiMongodb } from 'react-icons/si'
+
 export default function Project() {
-    return <div></div>
+    return (
+        <div className="mt-16 flex justify-evenly">
+            <div className="card w-5/12 bg-base-100 bg-transparent shadow-xl">
+                <figure className="cursor-pointer">
+                    <Image
+                        width={407}
+                        height={228}
+                        src="https://github.com/felipeJJ/e-commerce-admin/assets/43899843/2191667b-f267-4931-a97c-2cf8f146abe5"
+                        alt="projeto 1"
+                        onClick={() =>
+                            window.open(
+                                'https://e-commerce-website-omega-two.vercel.app/',
+                            )
+                        }
+                    />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title justify-center">
+                        E-commerce Website
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <div className="card-actions mt-4 w-full justify-center">
+                        <div className="badge w-1/2 justify-between border-hidden bg-transparent">
+                            <RiNextjsLine size={25} />
+                            <BiLogoTypescript size={25} />
+                            <BiLogoNodejs size={25} />
+                            <SiMongodb size={25} />
+                            <BiLogoTailwindCss size={25} />
+                        </div>
+                    </div>
+                    <div className="card-actions mt-5 justify-center">
+                        <button
+                            onClick={() =>
+                                window.open(
+                                    'https://github.com/felipeJJ/e-commerce-website',
+                                )
+                            }
+                            className="btn border-stone-600 bg-transparent shadow-xl"
+                        >
+                            <RiGithubFill size={20} />
+                            Visite o projeto
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="card w-5/12 bg-base-100 bg-transparent shadow-xl">
+                <figure>
+                    <Image
+                        width={407}
+                        height={228}
+                        src="https://github.com/felipeJJ/e-commerce-admin/assets/43899843/0ca79455-7cd5-4b76-8a01-aa7501bca1c7"
+                        alt="projeto 2"
+                    />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title justify-center">
+                        E-commerce Admin
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <div className="card-actions mt-4 w-full justify-center">
+                        <div className="badge w-1/2 justify-between border-hidden bg-transparent">
+                            <RiNextjsLine size={25} />
+                            <BiLogoTypescript size={25} />
+                            <BiLogoNodejs size={25} />
+                            <SiMongodb size={25} />
+                            <BiLogoTailwindCss size={25} />
+                        </div>
+                    </div>
+                    <div className="card-actions mt-5 justify-center">
+                        <button
+                            onClick={() =>
+                                window.open(
+                                    'https://github.com/felipeJJ/e-commerce-admin',
+                                )
+                            }
+                            className="btn border-stone-600 bg-transparent shadow-xl"
+                        >
+                            <RiGithubFill size={20} />
+                            Visite o projeto
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
